078b924df2fc32fe4c01f322072f4afacb5714aa
Alexandros Kalimeris <kalimerisalx@gmail.com>
2016-07-24 23:27:16 +0300

    Session Tests

---
+++ API/chat/tests/sessions/test_login.py
@@ -0,0 +1,64 @@
+#WORK IN PROGRESS
+class SessionTests(TestCase):
+    def test_login_with_invalid_username(self):
+        post_dict = {'username': '', password: 'bigfatsecret'}
+        response = self.client.post(
+            reverse('chat:sessions'),
+            post_dict
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_login_with_reserved_username_and_no_password(self):
+        post_dict = {'username': 'alex', password: ''}
+        response = self.client.post(
+            reverse('chat:sessions'),
+            post_dict
+        )
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_login_with_reserved_username_and_wrong_password(self):
+        post_dict = {'username': 'alex', password: 'wrongpass'}
+        response = self.client.post(
+            reverse('chat:sessions'),
+            post_dict
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_login_with_reserved_username_and_correct_password(self):
+        post_dict = {'username': 'alex', password: 'correctpass'}
+        response = self.client.post(
+            reverse('chat:sessions'),
+            post_dict
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(self.client.session.get('ting_auth'))
+
+    def test_login_with_not_reserved_username_and_password(self):
+        post_dict = {'username': 'alex', password: 'bigfatsecret'}
+        response = self.client.post(
+            reverse('chat:sessions'),
+            post_dict
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_login_wih_not_reserved_username_and_no_password(self):
+        post_dict = {'username': 'alex', password: 'bigfatsecret'}
+        response = self.client.post(
+            reverse('chat:sessions'),
+            post_dict
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(self.client.session.get('ting_auth'))
+
+    def logout_reserved_user(self):
+        response = self.client.delete(
+            reverse('chat:sessions', args=(message.id,)),
+            {}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(self.client.session.get('ting_auth'))
+
+
+
+
